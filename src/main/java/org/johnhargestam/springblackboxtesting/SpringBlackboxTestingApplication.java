db9232ed069e41e1c58a473757060e042855b965
= <john.hargestam@gmail.com>
2021-09-09 22:39:47 +0200

    initial commit

---
+++ src/main/java/org/johnhargestam/springblackboxtesting/SpringBlackboxTestingApplication.java
@@ -0,0 +1,13 @@
+package org.johnhargestam.springblackboxtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlackboxTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlackboxTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/johnhargestam/springblackboxtesting/SpringBlackboxTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.johnhargestam.springblackboxtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlackboxTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
